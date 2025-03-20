30755f25aee1ca381df0b9e104c1525a96e619ca
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-03-20 13:32:12 -0700

    Fix bug that was affecting price filtering

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -145,13 +145,13 @@ const getAllProperties = (options, limit = 10) => {
   }
 
   if (options.minimum_price_per_night) {
-    queryParams.push(options.minimum_price_per_night * 100);
-    queryString += ` AND cost_per_night >= $${queryParams.length}`;
+    queryParams.push(options.minimum_price_per_night);
+    queryString += ` AND cost_per_night/100 >= $${queryParams.length}`;
   }
 
   if (options.maximum_price_per_night) {
-    queryParams.push(options.maximum_price_per_night * 100);
-    queryString += ` AND cost_per_night <= $${queryParams.length}`;
+    queryParams.push(options.maximum_price_per_night);
+    queryString += ` AND cost_per_night/100 <= $${queryParams.length}`;
   }
 
   queryString += `
